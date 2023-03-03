1a48b4b006937a7860179faacf3a8417fa4df68e
makarovyarind <1makarov.yaroslav@gmail.com>
2023-03-03 18:30:32 +0200

    first commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const cardsMarcup = createGelleryItemMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', cardsMarcup);
+
+function createGelleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+    })
+    .join('');
+};
+
+let gallery = new SimpleLightbox('.gallery__item', {
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: 'bottom',
+    captionDelay: 250,  
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
+LOCAL_KEY = 'feedback-form-state'; 
 
+let formData = {};
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector(".feedback-form input"),
+    textarea: document.querySelector(".feedback-form textarea")
+};
+
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+savedText();
+
+function savedText() {
+    let data = localStorage.getItem(LOCAL_KEY);
+    
+    if (data) {
+        
+        formData = JSON.parse(data);
+    refs.input.value = formData.email || '';
+    refs.textarea.value = formData.message || '';
+    }
+}
+
+function onFormSubmit(evt) {
+
+    console.log(formData);
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(LOCAL_KEY);
+}
+
+function onFormInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+    
+}
